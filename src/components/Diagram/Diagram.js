d0d9a0ce9cc1550d6081d7fa6edbbab1ca77a376
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2021-04-20 11:04:30 -0400

    Stroy 1: Layout

--- src/components/Diagram/Diagram.js
+++ src/components/Diagram/Diagram.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import { Box, makeStyles } from '@material-ui/core';
-import * as d3 from 'd3';
-import { ReactComponent as SVGDiagram } from '../../assets/images/diagram.svg';
+//import * as d3 from 'd3';
+//import { ReactComponent as SVGDiagram } from '../../assets/images/diagram.svg';
 
-export const useStyles = makeStyles(() => ({ root: { padding: 30 } }));
+export const useStyles = makeStyles(() => ({ root: { padding: 10, height: '58vh' }, inner: {padding:10, border: 'solid'} }));
 
 const Diagram = () => {
     const classes = useStyles();
 
     React.useEffect(() => {
-        d3.select('g')
-            .selectAll('*')
-            .on('click', function() {
-                console.log(this);
-                d3.select(this)
-                    .attr('stroke', 'tomato');
-            })
-            .attr('pointer-events', 'fill')
-            .style('cursor', 'pointer');
+
     }, []);
 
     return (
         <Box
-            alignItems='center'
             className={classes.root}
             display='flex'
             flexDirection='row'
+            justifyContent = 'space-between'
         >
-            <SVGDiagram />
+            <Box
+                color = '"secondary.main"'
+                className={classes.inner}
+                alignItems='left'
+                width = {0.49}
+                >   
+            </Box>
+            <Box
+                color = '"secondary.main"'
+                className={classes.inner}
+                alignItems='right'
+                width = {0.49}
+            >
+            </Box>
         </Box>
     );
 };
