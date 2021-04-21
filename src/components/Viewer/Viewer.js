5a2a2c393ee3fe91cced94fa7dfa3063c5fa4b7f
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2021-04-21 16:28:00 -0400

    [RC 339] - Button

--- src/components/Viewer/Viewer.js
+++ src/components/Viewer/Viewer.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Box, makeStyles } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import ReplayIcon from '@material-ui/icons/Replay';
+import Button from '../Button';
+
 
 export const useStyles = makeStyles(() => ({
     root: {
@@ -26,6 +32,26 @@ export const useStyles = makeStyles(() => ({
         margin: 5,
         minWidth: 450,
     },
+    loadButton: {
+        position: "absolute",
+        bottom: 0,
+        right: 0,
+        margin: 5,
+    },
+    assignButton: {
+        position: 'absolute', 
+        margin: 5,
+    },
+    downloadButton: {
+        position: 'absolute',
+        right: 0,
+        margin: 5,
+    },
+    replayButton: {
+        position: 'absolute',
+        right: 35,
+        margin: 5
+    }
 }));
 
 const Viewer = () => {
@@ -38,10 +64,48 @@ const Viewer = () => {
         >
             <Box
                 className={classes.jsonViewer}
-            />
+            >
+                <Button
+                    className={classes.loadButton}
+                    color = 'primary'
+                >
+                    load json
+                </Button>
+                
+            </Box>
             <Box
                 className={classes.svgViewer}
-            />
+            >
+                <Button
+                    className={classes.loadButton}
+                    color = 'primary'
+                    >
+                        load svg
+                </Button>
+
+                <Button
+                    className={classes.assignButton}
+                    color='primary'
+                    endIcon={<CheckCircleIcon></CheckCircleIcon>}
+                    variant='contained'
+                    >
+                        assign
+                </Button>
+
+                <IconButton
+                    className={classes.downloadButton}
+                    color = 'primary'
+                >
+                    <GetAppIcon />        
+                </IconButton>
+
+                <IconButton
+                    className={classes.replayButton}
+                    color='primary'
+                    >
+                        <ReplayIcon />
+                </IconButton>
+            </Box>
         </Box>
     );
 };
